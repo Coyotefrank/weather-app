2f02dc607b73e6649609179865fc294af825bb05
Coyotefrank <franklopez35789@gmail.com>
2023-07-11 15:09:15 -0700

    created psudocode and all files

---
+++ Assets/script.js
@@ -0,0 +1,22 @@
+//global variables, what needs to be stored and referenced in
+//multipul functions
+//request URLs with specefic variable names
+// store API keys
+
+  //batch all of the dom element selectors
+
+// functions
+  //checking local storage 
+
+  // create search function  (can be broken upinto multiple functions)
+     //take in what was typed in the input field
+     //fetch request for coordinets with API
+     // convert to JSON format
+     //recive back the parse returned data
+     //2nd fetch request , use coodanits from first fetch to fetch weather data from API
+     // convert to JSON format
+     //extract temp,wind and humidity =5 day forcast from object data
+     //display data to page
+
+//creat a function for storing searches
+  // update/call recent searched function
