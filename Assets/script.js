ec9ab6741ccb6d1b95101df5dc2dae9ec73c52fc
Coyotefrank <franklopez35789@gmail.com>
2023-07-13 22:30:36 -0700

    added my weather apis and made alot of changes to my javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,78 @@
+var resultsContainer = document.querySelector(".container2");
+function renderWeather(weathers) {
+  var url=`https://api.openweathermap.org/data/2.5/forecast?lat=${weathers.coord.lat}&lon=${weathers.coord.lon}&appid=f7782879ec117ba6cf0a4aa3ad7235e4`;
+  fetch(url)
+  .then(response => response.json())
+  .then(data => fiveDaysDisplay(data.list));
+  console.log(weathers);
+  console.log("result on line 4", resultsContainer);
+  
+  var city = document.createElement("p");
+  city.textContent = weathers.name;
+  //resultsContainer.appened(city);
+  //document.querySelector("body").append(city);
+  //console.log("result on line 9", resultsContainer);
+
+  var temp = document.createElement("p");
+  temp.textContent = "temp: " + weathers.main.temp + "F";
+ // resultsContainer.appened(temp);
+  console.log("result on line 14", temp);
+  var visibility = document.createElement("p");
+  visibility.textContent = "Visibility " + weathers.visibility + "%";
+  console.log("result on line 18", visibility);
+  city.append(temp);
+  //document.querySelector(".container2").append(city);
+}
+function fetchWeather(query) {
+  var url="https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=f7782879ec117ba6cf0a4aa3ad7235e4";
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => renderWeather(data));
+}
+function fiveDaysDisplay(data) {
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    if (element.dt_txt.includes("00:00:00")) {
+      console.log(element)
+    }
+  }
+
+}
+
+function searchWeather() {
+  var city = document.getElementById("cityInput").value;
+  var apiKey = '249be4de6fc1f3ced3639f3f0e602786'; // Replace with your actual API key
+  var weatherContainer = document.getElementById("weatherContainer");
+
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=New%20York&appid=249be4de6fc1f3ced3639f3f0e602786`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      // Extract relevant weather information from the API response
+      var location = data.location.name;
+      var temperature = data.current.temp;
+      var condition = data.current.condition.text;
+
+      // Display the weather information in the weatherContainer div
+      weatherContainer.innerHTML = `
+        <h2>Weather in ${location}</h2>
+        <p>Temperature: ${temperature}°C</p>
+        <p>Condition: ${condition}</p>
+      `;
+    })
+    .catch(error => {
+      // Handle any errors that occur during the API request
+      console.error('Error fetching weather data:', error);
+      weatherContainer.innerHTML = 'An error occurred while fetching weather data.';
+    });
+  }
+fetchWeather();
+
+
 //global variables, what needs to be stored and referenced in
-var container1,document=queryselector("container1")
-var container2,document=queryselector("continer2")
+// var container1,document=queryselector("container1")
+// var container2,document=queryselector("continer2")
 //multipul functions
 //request URLs with specefic variable names
 // store API keys
@@ -13,32 +85,5 @@ var container2,document=queryselector("continer2")
   // create search function  (can be broken upinto multiple functions)
      //take in what was typed in the input field
   //   var apiKey = 'YOUR_API_KEY';
-
-// var apiUrl = 'https://api.weatherapi.com/v1/current.json?q=London&key=' + apiKey;   
-//      fetch request for coordinets with API
-     
-//   fetch(apiUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     // Handle the API response data here
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     // Handle any errors that occur during the API request
-//     console.error('Error:', error);
-//   });
-     // convert to JSON format
-     //recive back the returned data
-     //2nd fetch request , use coodanits from first fetch to fetch weather data from API
-     // convert to JSON format
-     //extract temp,wind and humidity =5 day forcast from object data
-     //display data to page
-
-//creat a function for storing searches
-  // update/call recent searched function
-
-
-//   var apiKey = 'YOUR_API_KEY';
-// var apiUrl = 'https://api.weatherapi.com/v1/current.json?q=London&key=' + apiKey;
 
 
