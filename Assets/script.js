b15cf30f274582cc1954ecb8d554ba2ef964d460
Coyotefrank <franklopez35789@gmail.com>
2023-07-14 22:34:42 -0700

    tried to add the icons and other display using javascript and making additional changes to the html

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
-var resultsContainer = document.querySelector(".container2");
+
+var resultsContainer = document.querySelector(".weatherContainer");
 function renderWeather(weathers) {
   var url=`https://api.openweathermap.org/data/2.5/forecast?lat=${weathers.coord.lat}&lon=${weathers.coord.lon}&appid=f7782879ec117ba6cf0a4aa3ad7235e4`;
   fetch(url)
@@ -10,12 +11,12 @@ function renderWeather(weathers) {
   var city = document.createElement("p");
   city.textContent = weathers.name;
   //resultsContainer.appened(city);
-  //document.querySelector("body").append(city);
-  //console.log("result on line 9", resultsContainer);
+  document.querySelector("body").append(city);
+  console.log("result on line 9", resultsContainer);
 
   var temp = document.createElement("p");
   temp.textContent = "temp: " + weathers.main.temp + "F";
- // resultsContainer.appened(temp);
+  resultsContainer.append(temp);
   console.log("result on line 14", temp);
   var visibility = document.createElement("p");
   visibility.textContent = "Visibility " + weathers.visibility + "%";
@@ -23,12 +24,15 @@ function renderWeather(weathers) {
   city.append(temp);
   //document.querySelector(".container2").append(city);
 }
-function fetchWeather(query) {
-  var url="https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=f7782879ec117ba6cf0a4aa3ad7235e4";
+function fetchWeather(city) { 
+  var url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f7782879ec117ba6cf0a4aa3ad7235e4`;
 
   fetch(url)
   .then(response => response.json())
-  .then(data => renderWeather(data));
+  .then(data => {
+    console.log(data)
+    renderWeather(data)
+  });
 }
 function fiveDaysDisplay(data) {
   for (let i = 0; i < data.length; i++) {
@@ -38,36 +42,48 @@ function fiveDaysDisplay(data) {
     }
   }
 
+   for(i=0;i<5;i++){
+    var iconcContainer =document.getElementById("iconcContainer-"+i);
+    iconcContainer.innerHTML+=`<img src="https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png">`;
+   
+   }
 }
+document.getElementById("searchForCity").addEventListener("click", function(event){
+  event.preventDefault()
+  var userInput=document.getElementById("searchInput")
+  fetchWeather(userInput.value)
+})
 
-function searchWeather() {
-  var city = document.getElementById("cityInput").value;
-  var apiKey = '249be4de6fc1f3ced3639f3f0e602786'; // Replace with your actual API key
-  var weatherContainer = document.getElementById("weatherContainer");
+// fetchWeather();
+// function searchWeather() {
+//   var city = document.getElementById("cityInput").value;
+//   var apiKey = '249be4de6fc1f3ced3639f3f0e602786'; // Replace with your actual API key
+//   var weatherContainer = document.getElementById("weatherContainer");
 
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=New%20York&appid=249be4de6fc1f3ced3639f3f0e602786`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      // Extract relevant weather information from the API response
-      var location = data.location.name;
-      var temperature = data.current.temp;
-      var condition = data.current.condition.text;
+//   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=New%20York&appid=249be4de6fc1f3ced3639f3f0e602786`)
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log(data)
+//       // Extract relevant weather information from the API response
+//       var location = data.location.name;
+//       var temperature = data.current.temp;
+//       var condition = data.current.condition.text;
 
-      // Display the weather information in the weatherContainer div
-      weatherContainer.innerHTML = `
-        <h2>Weather in ${location}</h2>
-        <p>Temperature: ${temperature}°C</p>
-        <p>Condition: ${condition}</p>
-      `;
-    })
-    .catch(error => {
-      // Handle any errors that occur during the API request
-      console.error('Error fetching weather data:', error);
-      weatherContainer.innerHTML = 'An error occurred while fetching weather data.';
-    });
-  }
-fetchWeather();
+//       // Display the weather information in the weatherContainer div
+//       weatherContainer.innerHTML = `
+//         <h2>Weather in ${location}</h2>
+//         <p>Temperature: ${temperature}°C</p>
+//         <p>Condition: ${condition}</p>
+//       `;
+//     })
+//   }
+    // .catch(error => {
+    //   // Handle any errors that occur during the API request
+    //   console.error('Error fetching weather data:', error);
+    //   weatherContainer.innerHTML = 'An error occurred while fetching weather data.';
+    // });
+  
+
 
 
 //global variables, what needs to be stored and referenced in
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # weather-app
+
+In this application I attempted to make a 5 day weather forcasting display with a city search function.
+The user first inputs the city that they want the 5 day forcast of and the forcast would appear on the right in seperate columns.
+Once the input is complete by selecting the search button the usear will find information such as tempeter and other weather related information, if a different city is desired simply replace the previous input with the newly desired city and the same display should happen only with the new citys info.
+
+
+credits
+https://chat.openai.com/
+https://www.w3schools.com/js/js_string_methods.asp
+https://openweathermap.org/forecast5 
+and google search.
+
+
+
